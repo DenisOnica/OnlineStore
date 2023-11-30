25af15b053765e2f473972b951c98e40001ae821
Denis Onica <denis.onica97@gmail.com>
2023-11-30 11:05:18 +0200

    navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Home } from "./Pages/Home";
 import { Cart } from "./Pages/Cart";
 import Admin from "./Pages/Admin";
 
-import NavBar from "./Components/NavBar";
+import NavBar from "./Components/Navbar";
 
 import { Routes, Route } from "react-router-dom";
 
