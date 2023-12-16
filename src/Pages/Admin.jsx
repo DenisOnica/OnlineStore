932b0e644062f5b5311e6f5ec54090d433729712
Denis Onica <denis.onica97@gmail.com>
2023-12-16 21:23:26 +0200

    the page updates after every request in the Admin page

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -22,7 +22,7 @@ const Admin = () => {
     fetchProducts();
   }, []);
 
-  const addNewProduct = () => {
+  const addNewProduct = async () => {
     const url = "https://652bdb8ed0d1df5273eecf9b.mockapi.io/products";
     const options = {
       method: "POST",
@@ -31,7 +31,44 @@ const Admin = () => {
       },
       body: JSON.stringify(product),
     };
-    fetch(url, options);
+
+    try {
+      const response = await fetch(url, options);
+      let updatedResponse;
+      if (response.ok) {
+        updatedResponse = await fetch(
+          "https://652bdb8ed0d1df5273eecf9b.mockapi.io/products"
+        );
+      }
+      const updatedProducts = await updatedResponse.json();
+      setProducts(updatedProducts);
+    } catch (error) {
+      console.log("An error appeared while updating product: ", error);
+    }
+  };
+
+  const editProduct = async (productID) => {
+    const url = `https://652bdb8ed0d1df5273eecf9b.mockapi.io/products/${productID}`;
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    };
+    try {
+      const response = await fetch(url, options);
+      let updatedResponse;
+      if (response.ok) {
+        updatedResponse = await fetch(
+          "https://652bdb8ed0d1df5273eecf9b.mockapi.io/products"
+        );
+      }
+      const updatedProducts = await updatedResponse.json();
+      setProducts(updatedProducts);
+    } catch (error) {
+      console.log("An error appeared while updating product: ", error);
+    }
   };
 
   const deleteProduct = async (productID) => {
@@ -53,69 +90,85 @@ const Admin = () => {
 
   return (
     <div className="p-10">
-      <div className="flex flex-col gap-4">
-        <div className="flex gap-10">
-          <label htmlFor="name">Name</label>
+      <div className="grid grid-cols-1 gap-4">
+        <div>
+          <label htmlFor="name" className="block text-sm font-medium">
+            Name
+          </label>
           <input
             type="text"
             name="name"
             id="name"
             value={product.name}
-            className=" border-black border-solid border-2"
+            className="mt-1 p-2 border border-gray-300 rounded-md"
             onChange={(e) => {
               const productName = e.target.value;
               setProduct({ ...product, name: productName });
             }}
           />
         </div>
-        <div className="flex gap-10">
-          <label htmlFor="imageURL">Image URL</label>
+        <div>
+          <label htmlFor="imageURL" className="block text-sm font-medium">
+            Image URL
+          </label>
           <input
             type="text"
             name="imageURL"
             id="imageURL"
             value={product.imageURL}
-            className=" border-black border-solid border-2"
+            className="mt-1 p-2 border border-gray-300 rounded-md"
             onChange={(e) => {
               const imageURL = e.target.value;
               setProduct({ ...product, imageURL: imageURL });
             }}
           />
         </div>
-        <div className="flex gap-10">
-          <label htmlFor="price">Price</label>
+        <div>
+          <label htmlFor="price" className="block text-sm font-mediums">
+            Price
+          </label>
           <input
             type="text"
             name="price"
             id="price"
             value={product.price}
-            className=" border-black border-solid border-2"
+            className="mt-1 p-2 border border-gray-300 rounded-md"
             onChange={(e) => {
               const productPrice = e.target.value;
               setProduct({ ...product, price: productPrice });
             }}
           />
         </div>
-        <div className="flex gap-10">
-          <label htmlFor="description">Description</label>
+        <div>
+          <label htmlFor="description" className="block text-sm font-medium">
+            Description
+          </label>
           <input
             type="text"
             name="description"
             id="description"
             value={product.description}
-            className=" border-black border-solid border-2"
+            className="mt-1 p-2 border border-gray-300 rounded-md"
             onChange={(e) => {
               const productDescription = e.target.value;
               setProduct({ ...product, description: productDescription });
             }}
           />
         </div>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-20"
-          onClick={addNewProduct}
-        >
-          Save
-        </button>
+        <div className="flex gap-3">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-20"
+            onClick={addNewProduct}
+          >
+            Add
+          </button>
+          <button
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full w-20"
+            onClick={() => editProduct(product.id)}
+          >
+            Save
+          </button>
+        </div>
       </div>
       <div>
         <table>
@@ -139,7 +192,7 @@ const Admin = () => {
                 <td>{product.description}</td>
                 <td>
                   <button
-                    className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                    className="border rounded-md py-2 px-4 mr-2"
                     id={product.id}
                     onClick={(e) => {
                       const productID = e.target.id;
@@ -156,7 +209,7 @@ const Admin = () => {
                 <td>
                   <button
                     id={product.id}
-                    className="bg-[red] text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                    className="border rounded-md py-2 px-4 mr-2 bg-red-600"
                     onClick={(e) => deleteProduct(e.target.id)}
                   >
                     Delete
