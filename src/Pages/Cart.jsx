cb030a7b3b2fb4d26195d5d9270aaa847db548a1
Denis Onica <denis.onica97@gmail.com>
2023-12-16 14:12:41 +0200

    feat: shopping cart using local storage

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export const Cart = () => {
   const [products, setProducts] = useState(null);
-  const [filteredProducts, setFilteredProducts] = useState(null);
+  const [productsInCart, setProductsInCart] = useState(
+    JSON.parse(window.localStorage.getItem("cart"))
+  );
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -11,7 +14,6 @@ export const Cart = () => {
       );
       const products = await response.json();
       setProducts(products);
-      setFilteredProducts(products);
     };
 
     fetchProducts();
@@ -22,22 +24,92 @@ export const Cart = () => {
     return product;
   };
 
-  const productsInCart = JSON.parse(window.localStorage.getItem("cart"));
+  const handleProductQuantity = (productsInCart, prodyctId, operator) => {
+    const currentProduct = productsInCart.find(
+      (product) => product.id === prodyctId
+    );
+    const indexOfProductToBeDeleted = productsInCart.indexOf(currentProduct);
+    switch (operator) {
+      case "decrease":
+        if (currentProduct.qt > 1) currentProduct.qt--;
+        break;
+      case "increase":
+        currentProduct.qt++;
+        break;
+      case "delete":
+        productsInCart.splice(indexOfProductToBeDeleted, 1);
+    }
 
-  return (
-    products && (
-      <div className="flex flex-col justify-center items-center">
-        {productsInCart.map((productInCart) => {
-          const product = getProductById(productInCart.id);
-          return (
-            <div className="flex" key={productInCart.id}>
-              <p>{product.name}</p>
-              <img width={100} src={product.imageURL} alt="product" />
-              <p>{product.price}</p>
+    if (productsInCart.length === 0) {
+      localStorage.removeItem("cart");
+      setProductsInCart(null);
+    } else {
+      localStorage.setItem("cart", JSON.stringify(productsInCart));
+      setProductsInCart(productsInCart);
+    }
+  };
+
+  const decreaseQuantity = (e) => {
+    const productsInCart =
+      JSON.parse(window.localStorage.getItem("cart")) ?? [];
+    handleProductQuantity(productsInCart, e.target.id, "decrease");
+  };
+
+  const increaseQuantity = (e) => {
+    const productsInCart =
+      JSON.parse(window.localStorage.getItem("cart")) ?? [];
+    handleProductQuantity(productsInCart, e.target.id, "increase");
+  };
+
+  const deleteQuantity = (e) => {
+    const productsInCart =
+      JSON.parse(window.localStorage.getItem("cart")) ?? [];
+    handleProductQuantity(productsInCart, e.target.id, "delete");
+  };
+
+  return products && productsInCart ? (
+    <div className=" p-10 gap-10 flex flex-col justify-center items-center">
+      {productsInCart.map((productInCart) => {
+        const product = getProductById(productInCart.id);
+        return (
+          <div
+            className="flex gap-10 justify-between items-center"
+            key={productInCart.id}
+          >
+            <p>{product.name}</p>
+            <img width={100} src={product.imageURL} alt="product" />
+            <p>{product.price}</p>
+            <div className="flex gap-5 items-center">
+              <button
+                id={productInCart.id}
+                className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
+                onClick={decreaseQuantity}
+              >
+                -
+              </button>
+              <p>{productInCart.qt}</p>
+              <button
+                id={productInCart.id}
+                className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
+                onClick={increaseQuantity}
+              >
+                +
+              </button>
+              <button
+                id={productInCart.id}
+                className="bg-red-400 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
+                onClick={deleteQuantity}
+              >
+                delete
+              </button>
             </div>
-          );
-        })}
-      </div>
-    )
+          </div>
+        );
+      })}
+    </div>
+  ) : (
+    <div>
+      Cosul este momentan gol , <Link to="/">adaugati produse</Link>
+    </div>
   );
 };
